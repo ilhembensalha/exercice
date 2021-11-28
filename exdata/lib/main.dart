d7219ac78576f0dfeebd837c21c89facd77e3599
ilhembensalha <bensalhailhem@gmail.com>
2021-11-29 00:43:19 +0100

    2

--- exdata/lib/main.dart
+++ exdata/lib/main.dart
@@ -31,11 +31,36 @@ class MyApp extends StatelessWidget {
             'Todo $i',
             'A description of what needs to be done for Todo $i',
           ),
+        ),
+      ),
     );
   }
 }
 
+class TodosScreen extends StatelessWidget {
+  // Requiring the list of todos.
+  const TodosScreen({Key? key, required this.todos}) : super(key: key);
 
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todos'),
+      ),
+      //passing in the ListView.builder
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+          );
+        },
+      ),
+    );
+  }
+}
 
 
 class Todo {
